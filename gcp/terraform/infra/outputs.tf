7f0be8cf8db63b473a6c4a762689f7f439635618
patrickjohncyh <patrickjohncyh@icloud.com>
2025-08-01 14:58:02 +0800

    get automatically get taint for gpu pool

--- gcp/terraform/infra/outputs.tf
+++ gcp/terraform/infra/outputs.tf
@@ -1,3 +1,14 @@
 output metaflow_workload_identity_gsa_id {
   value = google_service_account.metaflow_kubernetes_workload_identity_service_account.id
 }
+
+output metaflow_kubernetes_gpu_tolerations {
+  value = jsonencode([
+    for taint in google_container_node_pool.gpu_pool.node_config[0].taint : {
+      key      = taint.key
+      operator = "Equal"
+      value    = taint.value
+      effect   = lower(replace(taint.effect, "_", ""))
+    }
+  ])
+  }
